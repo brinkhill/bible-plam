86e4baa7a98646ba4486c0d58e32b26c834c7920
Brinkley Hill <8brink@gmail.com>
2023-08-10 21:50:58 -0400

    more stuff

---
+++ app/components/footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+ return <section className="flex justify-center">FOOTER</section>;
+}
---
+++ app/components/header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+ return <section className="flex justify-center">HEADER</section>;
+}
