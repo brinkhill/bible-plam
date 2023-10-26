44c5c5ba1769f03de6d851660d13af0223947d18
Brinkley Hill <8brink@gmail.com>
2023-10-25 21:51:23 -0400

    trying again

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,22 +7,22 @@ import localFont from "next/font/local";
 const openRunde = localFont({
   src: [
     {
-      path: "./fonts/OpenRunde-Regular.woff2",
+      path: "/fonts/OpenRunde-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/OpenRunde-Bold.woff2",
+      path: "/fonts/OpenRunde-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./fonts/OpenRunde-Medium.woff2",
+      path: "/fonts/OpenRunde-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts/OpenRunde-SemiBold.woff2",
+      path: "/fonts/OpenRunde-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
