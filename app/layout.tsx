0922b1be205d9691d181fc2078108f6a7c756b6e
Brinkley Hill <8brink@gmail.com>
2023-10-25 22:28:39 -0400

    does it work w inter?

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,27 +2,30 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import localFont from "next/font/local";
-const openRunde = localFont({
-  src: [
-    {
-      path: "./fonts/OpenRunde-Regular.woff",
-      weight: "400",
-    },
-    {
-      path: "./fonts/OpenRunde-Bold.woff",
-      weight: "700",
-    },
-    {
-      path: "./fonts/OpenRunde-Medium.woff",
-      weight: "500",
-    },
-    // {
-    //   path: "./fonts/OpenRunde-SemiBold.woff",
-    //   weight: "600",
-    // },
-  ],
-});
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
+// import localFont from "next/font/local";
+// const openRunde = localFont({
+//   src: [
+//     {
+//       path: "./fonts/OpenRunde-Regular.woff",
+//       weight: "400",
+//     },
+//     {
+//       path: "./fonts/OpenRunde-Bold.woff",
+//       weight: "700",
+//     },
+//     {
+//       path: "./fonts/OpenRunde-Medium.woff",
+//       weight: "500",
+//     },
+//     // {
+//     //   path: "./fonts/OpenRunde-SemiBold.woff",
+//     //   weight: "600",
+//     // },
+//   ],
+// });
 
 export const metadata: Metadata = {
   title: "Bible Plan",
@@ -37,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="theme-color" content="f7f8fc" />
-      <body className={openRunde.className}>
+      <body className={inter.className}>
         <Header />
         {children}
         <Footer />
